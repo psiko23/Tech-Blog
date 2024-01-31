434d8f494ca4b1c9ecbe7d3acc3ca609aef9e2e1
Psiko23 <tylerpittman2001@gmail.com>
2024-01-31 15:28:01 -0800

    initial commit/connection setup

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: process.env.SESSION_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
+};
+
+const hbs = exphbs.create();
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(session(sess));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended:true }));
+app.use(express.json());
+// app.use(routes);
+
+
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`App now listening`));
+});
